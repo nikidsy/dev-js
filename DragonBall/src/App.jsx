df3370634e01f856b7349bf10cf3dc2787abb1cc
nikidsy <dianaoliveirandrade@gmail.com>
2026-06-11 11:13:09 -0300

    feat: atualizar estrutura principal da aplicação

--- DragonBall/src/App.jsx
+++ DragonBall/src/App.jsx
@@ -1,12 +1,47 @@
 import './App.css'
-import { Header } from "./components/header/index.jsx";
-import {Main} from "./components/main/index.jsx";
+import {useState} from "react";
+import axios from "axios";
+// import { Header } from "./components/header/index.jsx";
+// import {Main} from "./components/main/index.jsx";
 
 function App() {
+
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [usuario, setUsuario] = useState(null)
+
+    const login = async () => {
+
+        try {
+            const resposta = await axios.post("http://localhost:3000/login", {email, senha})
+            const token = resposta.data.token;
+
+            localStorage.setItem("token", token)
+            alert("login realizado")
+        } catch {
+            alert("Login invalido")
+        }
+    }
+
+    const buscar = async () => {
+        const token = localStorage.getItem("token")
+        const resposta = await axios.get("http://localhost:3000/perfil", {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        setUsuario(resposta.data.usuario)
+    }
+
   return (
     <>
-      <Header />
-        <Main/>
+        <h2>Login</h2>
+        <input type="email" placeholder="informe seu e-mail" value={email} onChange={
+            (e) => setEmail(e.target.value)} />
+        <input type="password" placeholder="informe sua senha" value={senha} onChange={
+            (e) => setSenha(e.target.value)} />
+
+        <button onClick={login}>Entrar</button>
     </>
   )
 }
