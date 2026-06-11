2b615935b20c8bd38eb17f0c053c3a6fd8f5d895
nikidsy <dianaoliveirandrade@gmail.com>
2026-06-11 11:13:13 -0300

    feat: atualizar componente Main

--- DragonBall/src/components/main/index.jsx
+++ DragonBall/src/components/main/index.jsx
@@ -1,15 +1,39 @@
 import './style.css'
 import {Card} from "../card/index.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 export const Main = () => {
+
+    const [personagens, setPersonagens] = useState([]);
+
+    // useEffect(() => {
+    //     fetch("https://dragonball-api.com/api/characters?limit=58")
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             console.log(data.items[0])
+    //             setPersonagens(data.items);
+    //         })
+    //         .catch(err => console.log(err));
+    // }, []);
+
+    useEffect(() => {
+        axios.get("https://dragonball-api.com/api/characters?limit=58")
+            .then(res => {
+                setPersonagens(res.data.items)
+            })
+            .catch(err => console.log(err));
+    })
+
     return (
-        <>
             <main>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {personagens.map((elemento) => (
+                    <Card
+                    image={elemento.image}
+                    title={elemento.name}
+                    description={elemento.description}
+                    />
+                ))}
             </main>
-        </>
     )
 }
